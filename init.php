ee6e00ca36520dd479460a3e464d50d4d5b8e775
Sergey <nexxerr@gmail.com>
2018-03-16 11:17:34 +0300

    Update init.php

--- init.php
+++ init.php
@@ -4,18 +4,12 @@
     class CheckProductWeight {
         function RecalculateWeight($ID, &$arFields)
         {
-                    $arSelect = Array("ID", "IBLOCK_ID", "NAME", "DATE_ACTIVE_FROM", "CATALOG_WEIGHT");
-                    $arFilter = Array("IBLOCK_ID"=> IBLOCK_CATALOG_ID, "ID" => $arFields["ID"]);
-                    $dbFields = CIBlockElement::GetList(false, $arFilter, false, Array("nPageSize"=> 1), $arSelect);
-                    while($dbElement = $dbFields->GetNextElement())
-                    {
-                        $arResult = $dbElement->GetFields();
-                        if($arResult['CATALOG_WEIGHT'] || $arResult['CATALOG_WEIGHT'] != '') { // Проверка на наличие веса
-                        	$newWeight = $arResult['CATALOG_WEIGHT'] / 1000; // Пересчитываем вес и удаляем три нуля, потому что запятая нам не передаётся
-    						CIBlockElement::SetPropertyValuesEx($arFields["ID"], IBLOCK_CATALOG_ID, array("WEIGHT_GRAMS" => $newWeight)); // Записываем в свойство   
-                        }
-                    }           
-                
+                    /*dump($arFields);
+                    return false;*/
+                    if($arFields['WEIGHT'] || $arFields['WEIGHT'] != '') { // Проверка на наличие веса
+                        $newWeight = $arFields['WEIGHT'] / 1000; // Пересчитываем вес и удаляем три нуля, потому что запятая нам не передаётся
+                        CIBlockElement::SetPropertyValuesEx($arFields["ID"], IBLOCK_CATALOG_ID, array("WEIGHT_GRAMS" => $newWeight)); // Записываем в свойство   
+                    }       
         }
     }
 /* Recalculation product weight */
